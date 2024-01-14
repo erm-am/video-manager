37352fa30af2af62a7686c358322d16ab23dbcde
erm-am <b_ermakov_a@mail.ru>
2024-01-14 19:05:03 +0300

    [backend] videoCombine -> videoManager

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,14 +1,14 @@
 import path from 'path';
-import { videoCombine } from './services/video-combine/index.js';
+
 import { VIDEO_FOLDER_PATH } from './configs/core.config.js';
 import { PrismaClient } from '@prisma/client';
 import { server } from './server.js';
-// await videoCombine.merge({
+// await videoManager.merge({
 //   inputVideoFolderPath: VIDEO_FOLDER_PATH,
 //   outputVideoPath: path.resolve(process.cwd(), '1.mp4'),
 // });
 
-// await videoCombine.split({
+// await videoManager.split({
 //   inputVideoFilePath: path.resolve(process.cwd(), '1.mp4'),
 //   outputFolder: path.resolve(process.cwd(), 'output/'),
 //   maxDurationInSeconds: 60,
--- packages/backend/src/modules/video-manager/video-manager.service.ts
+++ packages/backend/src/modules/video-manager/video-manager.service.ts
@@ -37,7 +37,7 @@ const split = async (config: SplitConfig) => {
   }
 };
 
-export const videoCombine = {
+export const videoManager = {
   merge,
   split,
 };
